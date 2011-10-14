5be0c56e298c4a3f6c7772963fe36c845cbcc6b6
Graham Jones <grahamjones139@gmail.com>
2011-10-14 20:22:03 +0100

    Rehashing to include Bonfire code as a git submodule to make updating bonfire easier....

--- www/setFolderPerms.sh
+++ www/setFolderPerms.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-writeableFiles="../modules cache logs config archives db/backups db/migrations config/development config/production config/testing config/application.php config/config.php"
+writeableFiles="../modules cache logs config archives db/backups db/migrations config/development config/production config/testing config/application.php config/config.php config/database.php"
 cd bonfire/application
 
 for fname in $writeableFiles; do
